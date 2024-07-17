e68b65a9faf6794ccbc0a1ad188b35fbfeef65cb
dharini-21 <dhariniabi21@gmail.com>
2024-07-17 19:27:50 +0530

    Color Palette Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'smart-tunes-v2';
+  color="#fff"
+  @HostListener('touchmove', ['$event'])
+  handleSwipe(event:any){
+    let touch = event.touches[0] || event.changedTouches[0];
+    console.log(touch.pageX,touch.pageY);
+
+  }
+  // @HostListener('onmouseover', ['$event'])
+  hover(event:any){
+    console.log(event);
+    var target = event.target || event.srcElement
+    target.style.backgroundColor=this.getRandomColor();
+    event.stopPropagation()
+  }
+
+
+  counter(i: number) {
+    console.log(i)
+    return new Array(i);
+  }
+  getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    console.log(color)
+    return color;
+  }
 }
